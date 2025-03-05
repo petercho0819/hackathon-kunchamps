f1a610ae898e03cc130c8e501c49261479555d3b
youngseok <dongr52@naver.com>
2025-03-05 10:45:53 +0900

    img url 수정

--- asset/images/index.ts
+++ asset/images/index.ts
@@ -3,5 +3,5 @@ export const IMAGES = {
   // jisoo:
   //   "https://search.pstatic.net/common?type=b&size=3000&quality=100&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2FprofileImg%2F74993df6-eb16-4bfc-8065-e8f26f570ce6.jpg",
   vee: "https://search.pstatic.net/common?type=b&size=3000&quality=100&direct=true&src=http%3A%2F%2Fsstatic.naver.net%2Fpeople%2FprofileImg%2Ft%2Fb1ec63a2-d283-4bf7-bc16-80f360746125.png",
-  iu: "https://i.namu.wiki/i/aG4bbj6pYMiXHGkqQ8FRQOoZcukPcFJHAQ8qAVIGDerOvZ6GaRciLunfzX1F4ZvfFXpLbnV2pA2Jpu5wU88lNCgXcjExX9OpdDYfX9-UJ2NJ1iKDh5KJmIRq1_UbXI_5NmIJtL2_YLlhbRLpCnqAsQ.webp",
+  iu: "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyNDEyMDhfNjYg%2FMDAxNzMzNjIwNTU4MTg4.2x_9HCUSZX1AsnrrbbLe2cXVijbcPLplejCiKQh7v2Ig.qpkiwLmtu963JyEXuSIBV89gU3FhmvBjw-mkFCC5ht8g.JPEG%2FIMG_4629.JPG&type=sc960_832",
 };
